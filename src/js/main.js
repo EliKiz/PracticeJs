e54f937fe3449862a6788a25cc5460c163e672b0
Ilya <ilia0806280@gmail.com>
2022-06-03 22:45:01 +0400

    Modal state

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,20 @@ import "./slider";
 import showModals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
+import changeModalState from './modules/changeModalState';
 // console.log('awd');
 
 window.addEventListener('DOMContentLoaded', () => { 
     
-    
+    let modalState = {    }; // состояние модального окна 
+
+    changeModalState(modalState);  // передаем объект 
+
     showModals();
     tabs( '.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
-    
-    forms();
+    forms(modalState);
+
+
 });
